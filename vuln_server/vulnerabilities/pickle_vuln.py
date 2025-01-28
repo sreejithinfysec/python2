42a8356739ecd9b8c1b9d2d8d6ca07bae6817c25
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 23:36:57 +0530

    Fixing vuln_server/vulnerabilities/pickle_vuln.py for finding 62

--- vuln_server/vulnerabilities/pickle_vuln.py
+++ vuln_server/vulnerabilities/pickle_vuln.py
@@ -21,17 +21,13 @@ def injection():
         if 'input_data' in request.form and request.form['input_data'] != '':
             try:
                 # Validate input
-                if not re.match("^[A-Za-z0-9+/]*={0,2}$", request.form['input_data']):
+                input_data = request.form['input_data']
+                if re.match("^[A-Za-z0-9+/]*={0,2}$", input_data):
+                    # Load base64 encoded pickle object
+                    pickle.loads(base64.b64decode(input_data.encode()))
+                    return "Successfully loaded pickle object"
+                else:
                     raise ValueError("Invalid input_data")
-                
-                # Sanitize input
-                clean_input_data = bleach.clean(request.form['input_data'])
-                
-                # Load base64 encoded pickle object
-                pickle.loads(
-                    base64.b64decode(clean_input_data.encode()))
-                    
-                return "Successfully loaded pickle object"
             except Exception as e:
                 logging.error("Server Error: {}".format(str(e)))
                 return "Server Error: {}:".format(str(e))
@@ -39,6 +35,7 @@ def injection():
             flash('No selected file')
             return redirect(request.url)
     return render_template('pickle.html')
+
 
 
 
