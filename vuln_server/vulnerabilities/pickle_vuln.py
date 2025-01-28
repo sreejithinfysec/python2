10ab180eea4b6f56a7b97796b4f0c63b702ddfdf
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 23:04:49 +0530

    Fixing vuln_server/vulnerabilities/pickle_vuln.py for finding 48

--- vuln_server/vulnerabilities/pickle_vuln.py
+++ vuln_server/vulnerabilities/pickle_vuln.py
@@ -8,26 +8,33 @@ import hvac
 
 class PickleVuln():
 
-def injection():
-    """Pickle object command injection/execution.
+    def injection():
+        """Pickle object command injection/execution.
 
-    This function will evaluate if the user includes a file or
-    a pickle base64 object and load the object.
-    """
-    if request.method == 'POST':
-        # Check if data is not empty, post forms has all params defined
-        # which may be empty and cause unexpected behaviour
-        if 'input_data' in request.form and request.form['input_data'] != '':
-            try:
-                # Load base64 encoded pickle object
-                pickle.loads(
-                    base64.b64decode(request.form['input_data'].encode()))
-            except Exception as e:
-                return "Server Error: {}:".format(str(e))
-        else:
-            flash('No selected file')
-            return redirect(request.url)
-    return render_template('pickle.html')
+        This function will evaluate if the user includes a file or
+        a pickle base64 object and load the object.
+        """
+        if request.method == 'POST':
+            # Check if data is not empty, post forms has all params defined
+            # which may be empty and cause unexpected behaviour
+            if 'input_data' in request.form and request.form['input_data'] != '':
+                try:
+                    # Load base64 encoded pickle object
+                    pickle.loads(
+                        base64.b64decode(request.form['input_data'].encode()))
+                except Exception as e:
+                    return "Server Error: {}:".format(str(e))
+            elif 'file' in request.files and request.files['file'].filename != '':
+                file_data = request.files['file'].read()
+                try:
+                    pickle.loads(base64.b64decode(file_data))
+                except Exception as e:
+                    return "Server Error: {}:".format(str(e))
+            else:
+                flash('No selected file')
+                return redirect(request.url)
+        return render_template('pickle.html')
+
 
 
 
