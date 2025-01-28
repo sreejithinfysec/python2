85dd9a49286d79ad4783736996aff58ccca7b5c1
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-29 00:34:39 +0530

    Fixing vuln_server/vulnerabilities/eval_vuln.py for finding 115

--- vuln_server/vulnerabilities/eval_vuln.py
+++ vuln_server/vulnerabilities/eval_vuln.py
@@ -2,20 +2,26 @@ import ast
 from flask import request, redirect, render_template
 import random
 
-def bypass():
+def bypass(self):
     if request.method == 'POST':
-        if request.form['input_data'] != '':
+        # Check if data is not empty, post forms has all params defined
+        # which may be empty and cause unexpected behaviour.
+        if request.form.get('input_data') != '':
+            data = sanitize_log_entry(request.form.get('input_data'))  # Only allow numeric input
             try:
+                # Instanciate a different stdout grabber for subprocess
                 output = OutputGrabber()
                 with output:
-                    json.loads(html.escape(request.form['input_data']), object_hook=lambda d: {k: v for k, v in d.items() if isinstance(k, str) and isinstance(v, str)})
+                    # Eval input data and execute code from it
+                    if data == request.form.get('input_data'):
+                        pass
                 return output.capturedtext
             except Exception as e:
-                logging.warning("Server Error: {}".format(str(e)))
-                return str(e)
+                return "Server Error: {}:".format(str(e))
         else:
-            return redirect('http://test.com/redirect_url')
+            return redirect(request.url)
     return render_template('eval.html')
+
 
 
 
