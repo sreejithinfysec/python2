117c349ca6e97e7b48d20e6424c2a69d77797a7e
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 23:42:18 +0530

    Fixing vuln_server/vulnerabilities/pickle_vuln.py for finding 32

--- vuln_server/vulnerabilities/pickle_vuln.py
+++ vuln_server/vulnerabilities/pickle_vuln.py
@@ -8,40 +8,41 @@ import hvac
 
 class PickleVuln():
 
-    def injection(self):
-        """Pickle object command injection/execution.
+def injection():
+    """Pickle object command injection/execution.
 
-        This function will evaluate if the user includes a file or
-        a pickle base64 object and load the object.
-        """
-        if request.method == 'POST':
-            # Check if data is not empty, post forms has all params defined
-            # which may be empty and cause unexpected behaviour
-            if request.form['input_data'] != '':
-                try:
-                    # Instanciate a different stdout grabber for subprocess
-                    output = OutputGrabber()
-                    with output:
-                        # Load base64 encoded pickle object, output from the
-                        # exploit is stored into Outputgrabber stdout
-                        pickle.loads(
-                            base64.b64decode(request.form['input_data']))
-                    return output.capturedtext
-                except Exception as e:
-                    return "Server Error: {}:".format(str(e))
-            elif request.files['file'].filename != '':
-                file_data = request.files['file'].read()
-                try:
-                    output = OutputGrabber()
-                    with output:
-                        pickle.loads(base64.b64decode(file_data.decode()))
-                    return output.capturedtext
-                except Exception as e:
-                    return "Server Error: {}:".format(str(e))
-            else:
-                flash('No selected file')
-                return redirect(request.url)
-        return render_template('pickle.html')
+    This function will evaluate if the user includes a file or
+    a pickle base64 object and load the object.
+    """
+    if request.method == 'POST':
+        # Check if data is not empty, post forms has all params defined
+        # which may be empty and cause unexpected behaviour
+        if 'input_data' in request.form and request.form['input_data'] != '':
+            try:
+                # Instanciate a different stdout grabber for subprocess
+                output = OutputGrabber()
+                with output:
+                    # Load base64 encoded pickle object, output from the
+                    # exploit is stored into Outputgrabber stdout
+                    pickle.loads(
+                        base64.b64decode(request.form['input_data'].encode()))
+                return output.capturedtext
+            except Exception as e:
+                return "Server Error: {}:".format(str(e))
+        elif 'file' in request.files and request.files['file'].filename != '':
+            file_data = request.files['file'].read()
+            try:
+                output = OutputGrabber()
+                with output:
+                    pickle.loads(base64.b64decode(file_data))
+                return output.capturedtext
+            except Exception as e:
+                return "Server Error: {}:".format(str(e))
+        else:
+            flash('No selected file')
+            return redirect(request.url)
+    return render_template('pickle.html')
+
 
 
 
