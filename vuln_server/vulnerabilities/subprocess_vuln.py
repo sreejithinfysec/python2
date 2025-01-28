75777cc664007ed207565e7a08dac9c2d58deddb
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 22:32:23 +0530

    Fixing vuln_server/vulnerabilities/subprocess_vuln.py for finding 30

--- vuln_server/vulnerabilities/subprocess_vuln.py
+++ vuln_server/vulnerabilities/subprocess_vuln.py
@@ -6,7 +6,7 @@ from flask import request, redirect, render_template
 
 class SubprocessVuln():
 
-    def bypass(self):
+def bypass(self):
         if request.method == 'POST':
             # Check if data is not empty, post forms has all params defined
             # which may be empty and cause unexpected behaviour.
@@ -15,12 +15,12 @@ class SubprocessVuln():
                     # Instanciate a different stdout grabber for subprocess
                     output = OutputGrabber()
                     with output:
-                        # Execute system command with an unsafe input parameter
-                        subprocess.call("ping -c1 " +
-                                        request.form['input_data'], shell=True)
+                        # Execute system command with a safe input parameter
+                        subprocess.call(["ping", "-c1", request.form['input_data']])
                     return output.capturedtext
                 except Exception as e:
                     return "Server Error: {}:".format(str(e))
             else:
                 return redirect(request.url)
         return render_template('subprocess.html')
+
