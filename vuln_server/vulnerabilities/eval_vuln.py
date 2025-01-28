355b4ee0ac0c2b3154dd678b8afc67cc3be2f3fb
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 22:35:06 +0530

    Fixing vuln_server/vulnerabilities/eval_vuln.py for finding 31

--- vuln_server/vulnerabilities/eval_vuln.py
+++ vuln_server/vulnerabilities/eval_vuln.py
@@ -8,24 +8,20 @@ import ast
 from flask import request, redirect, render_template
 
 def bypass(self):
-    if request.method == 'POST':
-        # Check if data is not empty, post forms has all params defined
-        # which may be empty and cause unexpected behaviour.
-        if request.form.get('input_data') != '':
-            data = random.SystemRandom().randint(1, 1000)
-            try:
-                # Instanciate a different stdout grabber for subprocess
-                output = OutputGrabber()
-                with output:
-                    # Eval input data and execute code from it
-                    if data != int(request.form['input_data']):
-                        pass
-                return output.capturedtext
-            except Exception as e:
-                return "Server Error: {}:".format(str(e))
-        else:
-            return redirect(request.url)
-    return render_template('eval.html')
+        if request.method == 'POST':
+            if request.form['input_data'] != '':
+                try:
+                    output = OutputGrabber()
+                    with output:
+                        if random.randint(1, 1000) != json.loads(request.form['input_data']):
+                            pass
+                    return output.capturedtext
+                except Exception as e:
+                    return "Server Error: {}:".format(str(e))
+            else:
+                return redirect(request.url)
+        return render_template('eval.html')
+
 
 
 
