08d18695d082d2d9335470d6d0c116a671119aae
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 23:36:23 +0530

    Fixing vuln_server/vulnerabilities/eval_vuln.py for finding 65

--- vuln_server/vulnerabilities/eval_vuln.py
+++ vuln_server/vulnerabilities/eval_vuln.py
@@ -1,26 +1,32 @@
-import os
-from cryptography.fernet import Fernet
-from flask import request, redirect, render_template
-import random
-from vuln_server.outputgrabber import OutputGrabber
 import ast
 from flask import request, redirect, render_template
+import random
 
 def bypass(self):
     if request.method == 'POST':
         # Check if data is not empty, post forms has all params defined
         # which may be empty and cause unexpected behaviour.
         if request.form.get('input_data') != '':
+            data = random.SystemRandom().randint(1, 1000)
             try:
-                # Execute code from it
-                eval(sanitize_log_entry(request.form['input_data']))
-                return "Code executed successfully"
+                # Instanciate a different stdout grabber for subprocess
+                output = OutputGrabber()
+                with output:
+                    # Execute code from it
+                    code = ast.parse(request.form['input_data'], mode='exec')
+                    for element in code.body:
+                        if isinstance(element, ast.Expr):
+                            exec(compile(element.value, filename="<ast>", mode="exec"))
+                        elif isinstance(element, ast.Assign):
+                            exec(compile(element, filename="<ast>", mode="exec"))
+                return output.capturedtext
             except Exception as e:
                 return "Server Error: {}:".format(str(e))
 
         else:
             return redirect(request.url)
     return render_template('eval.html')
+
 
 
 
