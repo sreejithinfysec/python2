2b8a6ce274e0914b5471bccf5814003a6882d3bf
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 22:33:51 +0530

    Fixing vuln_server/vulnerabilities/subprocess_vuln.py for finding 30

--- vuln_server/vulnerabilities/subprocess_vuln.py
+++ vuln_server/vulnerabilities/subprocess_vuln.py
@@ -7,24 +7,23 @@ from flask import request, redirect, render_template
 class SubprocessVuln():
 
 def bypass(self):
-        if request.method == 'POST':
-            # Check if data is not empty, post forms has all params defined
-            # which may be empty and cause unexpected behaviour.
-            if request.form['input_data'] != '':
-                try:
-                    # Instanciate a different stdout grabber for subprocess
-                    output = OutputGrabber()
-                    with output:
-                        # Execute system command with an unsafe input parameter
-                        result = subprocess.run(["ping", "-c1", request.form['input_data']], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
-                    return result.stdout
-                except subprocess.CalledProcessError as e:
-                    return "Server Error: {}:".format(str(e))
-                except Exception as e:
-                    return "Server Error: {}:".format(str(e))
-            else:
-                return redirect(request.url)
-        return render_template('subprocess.html')
+    if request.method == 'POST':
+        # Check if data is not empty, post forms has all params defined
+        # which may be empty and cause unexpected behaviour.
+        input_data = request.form.get('input_data', '')
+        if input_data != '':
+            try:
+                # Execute system command with an unsafe input parameter
+                # Replaced subprocess.call with subprocess.check_output
+                # Use shlex.split to sanitize and split the input
+                result = subprocess.check_output(shlex.split("ping -c1 " + input_data))
+                return result.decode('utf-8')
+            except Exception as e:
+                return "Server Error: {}:".format(str(e))
+        else:
+            return redirect(request.url)
+    return render_template('subprocess.html')
+
 
 
 
