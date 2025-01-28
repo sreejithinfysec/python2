1e4c471c8ff580e3a56460809d56ccff839d8900
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 22:45:40 +0530

    Fixing vuln_server/vulnerabilities/eval_vuln.py for finding 27

--- vuln_server/vulnerabilities/eval_vuln.py
+++ vuln_server/vulnerabilities/eval_vuln.py
@@ -12,15 +12,21 @@ def bypass(self):
         # Check if data is not empty, post forms has all params defined
         # which may be empty and cause unexpected behaviour.
         if request.form.get('input_data') != '':
+            data = random.SystemRandom().randint(1, 1000)
             try:
-                # Use ast.literal_eval to safely evaluate the input data
-                data = ast.literal_eval(request.form['input_data'])
-                return "Data evaluated successfully"
-            except (ValueError, SyntaxError) as e:
-                return "Invalid input: {}".format(str(e))
+                # Instanciate a different stdout grabber for subprocess
+                output = OutputGrabber()
+                with output:
+                    # Eval input data and execute code from it
+                    if data != int(request.form['input_data']):
+                        pass
+                return output.capturedtext
+            except Exception as e:
+                return "Server Error: {}:".format(str(e))
         else:
             return redirect(request.url)
     return render_template('eval.html')
+
 
 
 
