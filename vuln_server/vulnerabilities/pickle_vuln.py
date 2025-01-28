1829cfbada389b948e1291a47f086184f2f233b3
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 23:08:18 +0530

    Fixing vuln_server/vulnerabilities/pickle_vuln.py for finding 48

--- vuln_server/vulnerabilities/pickle_vuln.py
+++ vuln_server/vulnerabilities/pickle_vuln.py
@@ -4,13 +4,10 @@ from vuln_server.outputgrabber import OutputGrabber
 from flask import flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 import os
+import hvac
+import bleach
 
 class PickleVuln():
-
-    def __reduce__(self):
-        # This method is called when an object is about to be serialized
-        # We return a tuple that represents the function to call and its arguments
-        return (self.__class__, ())
 
     def injection():
         """Pickle object command injection/execution.
@@ -23,26 +20,22 @@ class PickleVuln():
             # which may be empty and cause unexpected behaviour
             if 'input_data' in request.form and request.form['input_data'] != '':
                 try:
-                    # Load base64 encoded pickle object
+                    # Load base64 encoded pickle object after sanitizing the input
                     pickle.loads(
-                        base64.b64decode(request.form['input_data'].encode()),
-                        # Specify a safe_load function that only allows certain types of objects to be deserialized
-                        onload=[pickle.Unpickler],
-                        fiximports=True)
+                        base64.b64decode(bleach.clean(request.form['input_data']).encode()))
                 except Exception as e:
                     return "Server Error: {}:".format(str(e))
             elif 'file' in request.files and request.files['file'].filename != '':
                 file_data = request.files['file'].read()
                 try:
-                    pickle.loads(base64.b64decode(file_data),
-                                 onload=[pickle.Unpickler],
-                                 fiximports=True)
+                    pickle.loads(base64.b64decode(bleach.clean(file_data)))
                 except Exception as e:
                     return "Server Error: {}:".format(str(e))
             else:
                 flash('No selected file')
                 return redirect(request.url)
         return render_template('pickle.html')
+
 
 
 
