fc70a9a2e756d020d7c36e5070b03150b81e0374
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 22:30:17 +0530

    Fixing vuln_server/outputgrabber.py for finding 14

--- vuln_server/outputgrabber.py
+++ vuln_server/outputgrabber.py
@@ -36,6 +36,7 @@ def __enter__(self):
 
 
 
+
     def __exit__(self, type, value, traceback):
         self.stop()
 
@@ -54,6 +55,7 @@ def start(self):
         self.workerThread.start()
         # Make sure that the thread is running and os.read() has executed:
         time.sleep(0.01)
+
 
 
 
