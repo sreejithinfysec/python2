f09d141bd274d4df34d846e4244cd72708543b61
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 23:37:20 +0530

    Fixing vuln_server/vulnerabilities/eval_vuln.py for finding 43

--- vuln_server/vulnerabilities/eval_vuln.py
+++ vuln_server/vulnerabilities/eval_vuln.py
@@ -7,25 +7,16 @@ def bypass(self):
         # Check if data is not empty, post forms has all params defined
         # which may be empty and cause unexpected behaviour.
         if request.form.get('input_data') != '':
-            data = random.SystemRandom().randint(1, 1000)
             try:
-                # Instanciate a different stdout grabber for subprocess
-                output = OutputGrabber()
-                with output:
-                    # Execute code from it
-                    code = ast.parse(request.form['input_data'], mode='exec')
-                    for element in code.body:
-                        if isinstance(element, ast.Expr):
-                            exec(compile(element.value, filename="<ast>", mode="exec"))
-                        elif isinstance(element, ast.Assign):
-                            exec(compile(element, filename="<ast>", mode="exec"))
-                return output.capturedtext
-            except Exception as e:
-                return "Server Error: {}:".format(str(e))
-
+                # Use json.loads to safely evaluate the input data
+                data = json.loads(request.form['input_data'])
+                return "Data evaluated successfully"
+            except (ValueError, SyntaxError) as e:
+                return "Invalid input: {}".format(str(e))
         else:
             return redirect(request.url)
     return render_template('eval.html')
+
 
 
 
