4292c4160f58aa30a134662630edf259cebeefe8
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 22:26:04 +0530

    Fixing vuln_server/vulnerabilities/yaml_vuln.py for finding 14

--- vuln_server/vulnerabilities/yaml_vuln.py
+++ vuln_server/vulnerabilities/yaml_vuln.py
@@ -9,7 +9,7 @@ def injection(self):
     if request.method == 'POST':
         # Check if data is not empty, post forms has all params defined
         # which may be empty and cause unexpected behaviour
-        if request.form['input_data'] != '':
+        if 'input_data' in request.form and request.form['input_data'] != '':
             try:
                 # Instanciate a different stdout grabber for subprocess
                 output = OutputGrabber()
@@ -17,14 +17,11 @@ def injection(self):
                     # Load safe YAML input, output from the exploit
                     # is stored into Outputgrabber stdout
                     yaml_data = yaml.safe_load(request.form['input_data'])
-                    # Validate and authorize the deserialized data here
-                    # ...
-                    # Process the authorized data
-                    # ...
-                return output.capturedtext
+                    return yaml_data
             except Exception as e:
                 return "Server Error: {}:".format(str(e))
         else:
             return redirect(request.url)
     return render_template('yaml.html')
+
 
