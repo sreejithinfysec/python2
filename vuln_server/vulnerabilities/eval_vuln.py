af73a0b5e8a0da5eabbe452fb4ba5ca19e257c19
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 23:33:42 +0530

    Fixing vuln_server/vulnerabilities/eval_vuln.py for finding 63

--- vuln_server/vulnerabilities/eval_vuln.py
+++ vuln_server/vulnerabilities/eval_vuln.py
@@ -7,21 +7,28 @@ import ast
 from flask import request, redirect, render_template
 
 def bypass(self):
+    logger = get_logger()
     if request.method == 'POST':
         # Check if data is not empty, post forms has all params defined
         # which may be empty and cause unexpected behaviour.
-        if request.form.get('input_data') != '':
-            data = os.urandom(1).randint(1, 1000)
+        input_data = request.form.get('input_data')
+        if input_data and re.match("^[0-9]+$", input_data): # Validate input data is a number
+            data = flask.helpers.safe_join(random.SystemRandom(), range(1, 1000)) # Use safe_join to avoid path traversal
             try:
-                # Eval input data and execute code from it
-                if data != int(request.form['input_data']):
-                    pass
-                return "Server Error: Random number does not match input."
+                # Instanciate a different stdout grabber for subprocess
+                output = OutputGrabber()
+                with output:
+                    # Eval input data and execute code from it
+                    if data != int(input_data):
+                        pass
+                return output.capturedtext
             except Exception as e:
+                logger.error("Server Error: {}".format(str(e)))
                 return "Server Error: {}:".format(str(e))
         else:
             return redirect(request.url)
     return render_template('eval.html')
+
 
 
 
