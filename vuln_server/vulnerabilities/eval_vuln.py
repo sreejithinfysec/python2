e98d414d96c0b2bdcd4eea36cb17941922e2ab9a
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 22:26:56 +0530

    Fixing vuln_server/vulnerabilities/eval_vuln.py for finding 1

--- vuln_server/vulnerabilities/eval_vuln.py
+++ vuln_server/vulnerabilities/eval_vuln.py
@@ -1,23 +1,20 @@
+import os
+from cryptography.fernet import Fernet
+from flask import request, redirect, render_template
 import random
 
 from vuln_server.outputgrabber import OutputGrabber
-from flask import request, redirect, render_template
-
+import ast
 
 class EvalVuln():
 
     def bypass(self):
         if request.method == 'POST':
-            # Check if data is not empty, post forms has all params defined
-            # which may be empty and cause unexpected behaviour.
-            if request.form.get('input_data') != '':
-                data = random.SystemRandom().randint(1, 1000)
+            if request.form['input_data'] != '':
                 try:
-                    # Instanciate a different stdout grabber for subprocess
                     output = OutputGrabber()
                     with output:
-                        # Eval input data and execute code from it
-                        if data != eval(request.form['input_data']):
+                        if random.randint(1, 1000) != ast.literal_eval(request.form['input_data']):
                             pass
                     return output.capturedtext
                 except Exception as e:
@@ -25,6 +22,7 @@ class EvalVuln():
             else:
                 return redirect(request.url)
         return render_template('eval.html')
+
 
 
 
