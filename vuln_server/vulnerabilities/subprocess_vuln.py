c7f0ed6a527fdbbdfd8b390e01c110d31ade11e5
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 22:33:29 +0530

    Fixing vuln_server/vulnerabilities/subprocess_vuln.py for finding 30

--- vuln_server/vulnerabilities/subprocess_vuln.py
+++ vuln_server/vulnerabilities/subprocess_vuln.py
@@ -15,13 +15,16 @@ def bypass(self):
                     # Instanciate a different stdout grabber for subprocess
                     output = OutputGrabber()
                     with output:
-                        # Execute system command with a safe input parameter
-                        subprocess.call(["ping", "-c1", request.form['input_data']])
-                    return output.capturedtext
+                        # Execute system command with an unsafe input parameter
+                        result = subprocess.run(["ping", "-c1", request.form['input_data']], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+                    return result.stdout
+                except subprocess.CalledProcessError as e:
+                    return "Server Error: {}:".format(str(e))
                 except Exception as e:
                     return "Server Error: {}:".format(str(e))
             else:
                 return redirect(request.url)
         return render_template('subprocess.html')
+
 
 
