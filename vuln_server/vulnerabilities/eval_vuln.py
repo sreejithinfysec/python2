f4cdf8dc1b07832ea876595c7ed1569eb856e837
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 22:54:57 +0530

    Fixing vuln_server/vulnerabilities/eval_vuln.py for finding 34

--- vuln_server/vulnerabilities/eval_vuln.py
+++ vuln_server/vulnerabilities/eval_vuln.py
@@ -3,17 +3,14 @@ from flask import request, redirect, render_template
 
 def bypass(self):
     if request.method == 'POST':
-        # Check if data is not empty, post forms has all params defined
-        # which may be empty and cause unexpected behaviour.
         if request.form.get('input_data') != '':
             data = random.SystemRandom().randint(1, 1000)
             try:
-                # Eval input data and execute code from it
-                if data != eval(request.form['input_data']):
-                    pass
-                return "Code executed successfully"
+                result = interpreter(request.form['input_data'])
+                return result
             except Exception as e:
                 return "Server Error: {}:".format(str(e))
+
 
         else:
             return redirect(request.url)
