ca7bc7f8d0143bdc43b1b44b2ef05d4b1b7cec4a
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-01-28 23:44:44 +0530

    Fixing vuln_server/vulnerabilities/eval_vuln.py for finding 63

--- vuln_server/vulnerabilities/eval_vuln.py
+++ vuln_server/vulnerabilities/eval_vuln.py
@@ -4,16 +4,24 @@ import random
 
 def bypass(self):
     if request.method == 'POST':
-        if request.form['input_data'] != '':
+        # Check if data is not empty, post forms has all params defined
+        # which may be empty and cause unexpected behaviour.
+        if request.form.get('input_data') != '':
+            data = re.sub(r'\D', '', request.form.get('input_data'))  # Only allow numeric input
             try:
-                encoded_url = urlencode({'return_url': request.form['input_data']})
-                return redirect(url_for('.bypass') + '?' + encoded_url)
+                # Instanciate a different stdout grabber for subprocess
+                output = OutputGrabber()
+                with output:
+                    # Eval input data and execute code from it
+                    if data != request.form.get('input_data'):
+                        pass
+                return output.capturedtext
             except Exception as e:
                 return "Server Error: {}:".format(str(e))
         else:
-            encoded_url = urlencode({'return_url': request.url})
-            return redirect(url_for('.bypass') + '?' + encoded_url)
+            return redirect(request.url)
     return render_template('eval.html')
+
 
 
 
