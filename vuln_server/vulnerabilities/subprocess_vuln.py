5a72af16e16da3145e4a6a1f85da38f4f5a7b325
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-02-03 12:35:25 +0530

    Fixing vuln_server/vulnerabilities/subprocess_vuln.py for finding 13

--- vuln_server/vulnerabilities/subprocess_vuln.py
+++ vuln_server/vulnerabilities/subprocess_vuln.py
@@ -7,25 +7,28 @@ from flask import request, redirect, render_template
 class SubprocessVuln():
 
 def bypass(self):
-        if request.method == 'POST':
-            # Check if data is not empty, post forms has all params defined
-            # which may be empty and cause unexpected behaviour.
-            if request.form['input_data'] != '':
-                try:
-                    # Instanciate a different stdout grabber for subprocess
-                    output = OutputGrabber()
-                    with output:
-                        # Execute system command with an unsafe input parameter
-                        subprocess.run(["ping", "-c1", request.form['input_data']], 
-                                       check=True, 
-                                       stdout=subprocess.PIPE, 
-                                       stderr=subprocess.PIPE)
-                    return output.capturedtext
-                except subprocess.CalledProcessError as e:
-                    return "Server Error: {}:".format(str(e.stderr))
-            else:
-                return redirect(request.url)
-        return render_template('subprocess.html')
+    if request.method == 'POST':
+        # Check if data is not empty, post forms has all params defined
+        # which may be empty and cause unexpected behaviour.
+        if request.form['input_data'] != '':
+            # Validate and sanitize the user input
+            input_data = re.sub(r"[^\w\s]", '', request.form['input_data'])
+            try:
+                # Instanciate a different stdout grabber for subprocess
+                output = OutputGrabber()
+                with output:
+                    # Execute system command with sanitized input parameter
+                    subprocess.run(["ping", "-c1", input_data], shell=False, check=True)
+                return output.capturedtext
+            except subprocess.CalledProcessError as e:
+                # Use a logging library to handle log entries
+                logger = logging.getLogger(__name__)
+                logger.error("Server Error: %s", str(e))
+                return "Server Error: {}:".format(str(e))
+        else:
+            return redirect(request.url)
+    return render_template('subprocess.html')
+
 
 
 
