8e84d62ebb4ef282afa11b7a3f7e0c66193e6f12
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-02-03 12:35:57 +0530

    Fixing vuln_server/vulnerabilities/xml_vuln.py for finding 12

--- vuln_server/vulnerabilities/xml_vuln.py
+++ vuln_server/vulnerabilities/xml_vuln.py
@@ -7,22 +7,23 @@ from flask import request, redirect, render_template
 
 class XMLVuln():
 
-def injection():
+def injection(self):
     if request.method == 'POST':
         # Check if data is not empty, post forms has all params defined
         # which may be empty and cause unexpected behaviour
         if request.form['input_data'] != '':
-            # Instanciate an XML parser disabling unsafe external
+            # Instanciate an XML parser disallowing unsafe external
             # sources to be parsed by xml.parseString
             parser = make_parser()
             parser.setFeature(feature_external_ges, False)
-            doc = parseString(request.form['input_data'], parser=parser)
+            doc = dp.parseString(request.form['input_data'], parser=parser)
             for event, node in doc:
                 doc.expandNode(node)
                 return(node.toxml())
         else:
             return redirect(request.url)
     return render_template('xml.html')
+
 
 
 
