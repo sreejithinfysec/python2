5301378d043b5374aa3f5221492930c60c4fe2c9
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-02-03 12:33:02 +0530

    Fixing vuln_server/vulnerabilities/eval_vuln.py for finding 11

--- vuln_server/vulnerabilities/eval_vuln.py
+++ vuln_server/vulnerabilities/eval_vuln.py
@@ -5,22 +5,16 @@ import random
 def bypass(self):
     if request.method == 'POST':
         if request.form['input_data'] != '':
+            data = random.randint(1, 1000)
             try:
-                output = OutputGrabber()
-                with output:
-                    if random.randint(1, 1000) != json.loads(request.form['input_data']):
-                        pass
-                return output.capturedtext
+                if data != eval(re.sub(r'[\x00-\x1f\x7f-\x9f\\"\'\x08\x0c]', '', request.form['input_data'])):
+                    pass
             except Exception as e:
                 return "Server Error: {}:".format(str(e))
         else:
-            parsed_url = urlparse(request.url)
-            query_params = parse_qs(parsed_url.query)
-            next_url = query_params.get('next', [''])[0]
-            # Escape the URL before passing it to urlparse
-            escaped_next_url = urlencode({'next': next_url}, quote_via=quote_plus)
-            return redirect(escaped_next_url)
+            return redirect(request.url)
     return render_template('eval.html')
+
 
 
 
